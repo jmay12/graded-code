e422866f9ec936dad25bb885dca7e21ec7c35b7b
jmay12 <jmay12@msn.com>
2015-10-27 02:42:36 +0000

    HW2

--- multiply.php
+++ multiply.php
@@ -3,12 +3,12 @@
 $num = $_POST['numerator'];
 $denom = $_POST['denominator'];
 
-function division($x,$y) {
-  $result = $x / $y;
+function multiply($x,$y) {
+  $result = $x * $y;
   return $result;
   }
 
-echo "Answer is " . division($num,$denom) . ".";
+echo "Answer is " . multiply($num,$denom) . ".";
 echo "<br>";
 echo "<a href = 'http://jmay12php-136229.nitrousapp.com/graded-code/index2.php'>Go Back </a>";
 
